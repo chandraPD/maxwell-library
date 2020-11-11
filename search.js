7e9126059bf15a7ab39c6b994a79b4c895e9c425
candra <123456>
2020-11-11 16:33:14 +0700

    set Not Found Searching books and navbar home

--- search.js
+++ search.js
@@ -14,13 +14,20 @@ const books = document.querySelectorAll('.title-book');
 const searchBar = document.forms['search-books'].querySelector('input');
 searchBar.addEventListener('keyup', delay(function(e){
     const term = e.target.value.toLowerCase();
+    var countSuccess = 0;
     Array.from(books).forEach(function(book){
         const title = book.textContent;
 
         if(title.toLowerCase().indexOf(term) != -1){
             book.parentElement.parentElement.style.display = 'block';
+            countSuccess++;
         }else{
             book.parentElement.parentElement.style.display = 'none';
         }
     })
+    if(countSuccess == 0){
+        document.querySelector('.books-nf').style.display = 'block';
+    }else{
+        document.querySelector('.books-nf').style.display = 'none';
+    }
 },500));

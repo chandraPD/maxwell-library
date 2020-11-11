c000045d25fa92af26cea274de813e929ec78429
jskyte <jayson199732@gmail.com>
2020-11-11 10:40:09 +0700

    Separating one-page auth js into new file

--- auth/assets/js/script.js
+++ auth/assets/js/script.js
@@ -1,21 +1,21 @@
-var login = document.getElementById('login');
-var register = document.getElementById('register');
-var forgotPassword = document.getElementById('forgotPassword');
+    var login = document.getElementById("login");
+    var register = document.getElementById("register");
+    var forgotPassword = document.getElementById("forgotPassword");
 
-function displaySignUp() {
-  login.style.display = 'none';
-  register.style.display = 'block';
-  forgotPassword.style.display = 'none';
-}
+    function displaySignUp() {
+      login.style.display = "none";
+      register.style.display = "block";
+      forgotPassword.style.display = "none";
+    }
 
-function displayLogIn() {
-  login.style.display = 'block';
-  register.style.display = 'none';
-  forgotPassword.style.display = 'none';
-}
+    function displayLogIn() {
+      login.style.display = "block";
+      register.style.display = "none";
+      forgotPassword.style.display = "none";
+    }
 
-function displayForgotPassword() {
-  login.style.display = 'none';
-  register.style.display = 'none';
-  forgotPassword.style.display = 'block';
-}
+    function displayForgotPassword() {
+      login.style.display = "none";
+      register.style.display = "none";
+      forgotPassword.style.display = "block";
+    }

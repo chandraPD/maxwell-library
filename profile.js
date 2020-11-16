729e1801b17db441e0892565bc9be4ffc6c26666
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2020-11-16 22:57:40 +0700

    menambahkan sweet alert saat meng-click submit dan cancel pada edit form

--- profile.js
+++ profile.js
@@ -1,7 +1,63 @@
 const editButton = document.querySelector('button.edit-profile');
 const editForm = document.querySelector('#container-editform');
+const submitForm = document.querySelector('.btn-editform input');
+// const submitForm = document.querySelector('#submit-profile');
+const cancelForm = document.querySelector('.btn-editform button');
+
 
 
 editButton.addEventListener('click', function(){
     editForm.style.display = 'block';
 });
+
+submitForm.addEventListener('click', function(){
+    editForm.style.display = 'none';
+});
+
+submitForm.addEventListener('click', function() {
+    var firstName = document.getElementById('user-firstname').value;
+    var lastName = document.getElementById('user-lastname').value;
+    var dateOfBirth = document.getElementById('date-birth').value;
+    var emailUser = document.getElementById('user-email').value;
+    var addressUser = document.getElementById('user-address').value;
+    var phoneNumber = document.getElementById('user-number').value;
+
+    if (firstName == '' || lastName == '' || dateOfBirth == '' || emailUser == '' || addressUser == '' || phoneNumber == ''){
+        Swal.fire(
+            'Submit Failed !',
+            'You should fill in the blank',
+            'error'
+          );
+    }else{
+        Swal.fire(
+            'Submitted !',
+            'You clicked the button!',
+            'success'
+          );
+    }
+});
+
+cancelForm.addEventListener('click', function(){
+    editForm.style.display = 'none';
+});
+
+
+// UPDATE DATA USER 
+function submitEditProfile() {
+    editForm.style.display = 'none';
+
+    var firstName = document.getElementById('user-firstname').value;
+    var lastName = document.getElementById('user-lastname').value;
+    var dateOfBirth = document.getElementById('date-birth').value;
+    var emailUser = document.getElementById('user-email').value;
+    var addressUser = document.getElementById('user-address').value;
+    var phoneNumber = document.getElementById('user-number').value;
+
+    document.getElementById('firstname').innerHTML = firstName;
+    document.getElementById('lastname').innerHTML = lastName;
+    document.getElementById('birthday').innerHTML = dateOfBirth;
+    document.getElementById('email').innerHTML = emailUser;
+    document.getElementById('address').innerHTML = addressUser;
+    document.getElementById('phonenumber').innerHTML = phoneNumber;
+
+}

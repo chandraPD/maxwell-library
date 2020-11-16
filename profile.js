321efe24207b746c908b1cd0bc5873d5299cacc5
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2020-11-16 21:15:13 +0700

    menambahkan userprofil.html , tambahan css di style.css dan menambahkan file profile.js

---
+++ profile.js
@@ -0,0 +1,7 @@
+const editButton = document.querySelector('button.edit-profile');
+const editForm = document.querySelector('#container-editform');
+
+
+editButton.addEventListener('click', function(){
+    editForm.style.display = 'block';
+});

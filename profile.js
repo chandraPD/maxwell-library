9be66d6ba1bccaa03ed61a1e624ab8e245b3773a
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2020-11-17 16:29:10 +0700

    memperbaiki bagian profile.js bagian function submitEditProfile()

--- profile.js
+++ profile.js
@@ -12,25 +12,6 @@ submitForm.addEventListener('click', function(){
     editForm.style.display = 'none';
 });
 
-// UPDATE DATA USER 
-function submitEditProfile() {
-    editForm.style.display = 'none';
-
-    var firstName = document.getElementById('user-firstname').value;
-    var lastName = document.getElementById('user-lastname').value;
-    var dateOfBirth = document.getElementById('date-birth').value;
-    var emailUser = document.getElementById('user-email').value;
-    var addressUser = document.getElementById('user-address').value;
-    var phoneNumber = document.getElementById('user-number').value;
-
-    document.getElementById('firstname').innerHTML = firstName;
-    document.getElementById('lastname').innerHTML = lastName;
-    document.getElementById('birthday').innerHTML = dateOfBirth;
-    document.getElementById('email').innerHTML = emailUser;
-    document.getElementById('address').innerHTML = addressUser;
-    document.getElementById('phonenumber').innerHTML = phoneNumber;
-}
-
 submitForm.addEventListener('click', function() {
     var firstName = document.getElementById('user-firstname').value;
     var lastName = document.getElementById('user-lastname').value;
@@ -59,4 +40,23 @@ submitForm.addEventListener('click', function() {
 cancelForm.addEventListener('click', function(){
     editForm.style.display = 'none';
 });
+
+// UPDATE DATA USER 
+function submitEditProfile() {
+    editForm.style.display = 'none';
+
+    var firstName = document.getElementById('user-firstname').value;
+    var lastName = document.getElementById('user-lastname').value;
+    var dateOfBirth = document.getElementById('date-birth').value;
+    var emailUser = document.getElementById('user-email').value;
+    var addressUser = document.getElementById('user-address').value;
+    var phoneNumber = document.getElementById('user-number').value;
+
+    document.getElementById('firstname').innerHTML = firstName;
+    document.getElementById('lastname').innerHTML = lastName;
+    document.getElementById('birthday').innerHTML = dateOfBirth;
+    document.getElementById('email').innerHTML = emailUser;
+    document.getElementById('address').innerHTML = addressUser;
+    document.getElementById('phonenumber').innerHTML = phoneNumber;
+}
 

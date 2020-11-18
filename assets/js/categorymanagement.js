06d3ab21cb73c534740080b19790c7ca2e46cfa7
jskyte <jayson199732@gmail.com>
2020-11-18 11:39:20 +0700

    added sweetalert on edit category

--- assets/js/categorymanagement.js
+++ assets/js/categorymanagement.js
@@ -3,6 +3,10 @@
             swal("Success", "Your Category has been Added!", "success");
         }
 
+        function editSuccess() {
+            swal("Success", "Your Category has been Edited!", "success");
+        }
+
         $(document).ready(function () {
             $.validator.setDefaults({
                 submitHandler: function () {
@@ -44,3 +48,46 @@
                 }
             });
         });
+
+        $(document).ready(function () {
+            $.validator.setDefaults({
+                submitHandler: function () {
+                    editSuccess();
+                    $('#modal-edit').modal('hide');
+                    $('#modal-edit').on('hidden.bs.modal', function (e) {
+                        $(this).find('#editCategory')[0].reset();
+                    });
+                }
+            });
+            $('#editCategory').validate({
+                rules: {
+                    categoryId: {
+                        required: true,
+                    },
+                    categoryName: {
+                        required: true,
+                    }
+                },
+                messages: {
+                    categoryId: {
+                        required: "Please edit Category ID",
+                    },
+                    categoryName: {
+                        required: "Please edit Category Name",
+
+                    },
+                },
+                errorElement: 'span',
+                errorPlacement: function (error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function (element, errorClass, validClass) {
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight: function (element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid');
+                }
+            });
+        });
+

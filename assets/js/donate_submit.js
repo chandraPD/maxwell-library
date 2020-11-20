e36ce450457ff10fee74112f92370a3fa460a63b
candra <123456>
2020-11-20 11:45:32 +0700

    fix some donation js

--- assets/js/donate_submit.js
+++ assets/js/donate_submit.js
@@ -1,26 +1,24 @@
-const submitform= document.querySelector('btn-submitform');
-});
 //Submit Donate
 function submitdonate() {
-    form.form-horizontal.style.display = "none";
+    // event.preventDefault();
 
-    var Email = document.getElementById('inputEmail3')
-    var Name = document.getElementById('inputName3')
-    var Donation Type = document.getElementById('inputDonationtype3')
-    var Phone Number = document.getElementById('inputPhone3')
-    var Total Book by number = document.getElementById('inputBookbybook3')
+    var Email = document.getElementById('inputEmail').value;
+    var Name = document.getElementById('inputName').value;
+    var DonationType = document.getElementById('inputDonationtype').value;
+    var PhoneNumber = document.getElementById('inputPhone').value;
+    var DonationNumber = document.getElementById('inputBookbybook').value;
 
-    if (Email == ''|| Name == ''|| Donation Type=''|| Phone Number=''|| Total Book by number=''){
+    if (Email == ''|| Name == ''|| DonationType == ''|| PhoneNumber == ''|| DonationNumber == ''){
         Swal.fire(
             'Submit Failed !',
             'You should fill in the blank',
             'error'
         );
-        form.form-horizontal.style.display = "none";
     }else{
         Swal.fire(
             'Submitted !',
             'You clicked the button!',
             'success'
         );
-    }}
+    }
+}
